b66e88d350c8e21854bfff5cac8e1b6fea91cca6
Mücahit Arslan <marslan603@gmail.com>
2022-08-09 21:01:57 +0300

    Js var let const defines

--- FrontEnd/Java Script/Session-2/App.js
+++ FrontEnd/Java Script/Session-2/App.js
@@ -1,1 +1,42 @@
 console.log('App js Running')
+/*
+var a=5;
+console.log(a);
+
+{   
+    var a = 3;
+    console.log(a);
+}
+
+console.log(a);
+
+
+let b = 5;
+console.log(b);
+
+{
+    let b=3;
+    console.log(b);
+}
+console.log(b);
+
+console.log(typeof 0);
+console.log(typeof 3.14);
+console.log(typeof "Hello");
+console.log(typeof 'Hello');
+console.log(typeof (8>7)); 
+
+
+let c = +prompt('Bir Değer Giriniz');
+console.log( typeof typeof c);
+*/
+
+let age;
+
+console.log(age);
+
+comsole.log(myAge);
+
+var myAge = 40;
+
+console.log('Hello')
