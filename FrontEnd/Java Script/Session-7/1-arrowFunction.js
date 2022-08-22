c3557ce71e202962a55fcd1da52bfcb9ea37b940
Mücahit Arslan <marslan603@gmail.com>
2022-08-22 22:53:19 +0300

    Lesson_2_Start

--- FrontEnd/Java Script/Session-7/1-arrowFunction.js
+++ FrontEnd/Java Script/Session-7/1-arrowFunction.js
@@ -1,12 +1,120 @@
-// ? ===============================================
-// ?               FONKSİYONLAR
-// ? ===============================================
+// // ? ===============================================
+// // ?               FONKSİYONLAR
+// // ? ===============================================
 
-// !------------------------------------------------
-// ! 3.YONTEM  : ARROW FUNCTION
-// !------------------------------------------------
+// // !------------------------------------------------
+// // ! 3.YONTEM  : ARROW FUNCTION
+// // !------------------------------------------------
 
-console.log("**** ARROW ******");
+// console.log("**** ARROW ******");
 
-//* ORNEK:
-//************************************************/
+// //* ORNEK: Silindir Hacmi
+// //************************************************/
+
+// const r = Number(prompt("yaricap giriniz:"));
+// const h = Number(prompt("yukseklik giriniz:"));
+
+// const hacimHesapla = (r, h) => Math.PI * r * r * h;
+
+// // console.log(`${r} , ${h} => Hacmi: ${hacimHesapla(r, h).toExponential()}`);
+// console.log(`${r} , ${h} => Hacmi: ${hacimHesapla(r, h).toFixed(2)}`);
+
+// //* ORNEK: Yas hesapla
+// //************************************************/
+
+// const tarih = Number(prompt("Dogum Tarihini giriniz:"));
+
+// //! Arrow Func yontemi ile tanimlama
+// const yasHesapla = (tarih) => {
+//   const yas = new Date().getFullYear() - tarih;
+//   return yas;
+// };
+// console.log("YASINIZ:" + yasHesapla(tarih));
+
+// //* alternatif
+// const yasHesaplaKisa = (tarih) => new Date().getFullYear() - tarih;
+
+// // console.log("YASINIZ:" + yasHesapla(tarih));
+// console.log("YASINIZ:" + yasHesaplaKisa(tarih));
+
+// //?-----------------------------------------
+
+// // console.log("YASINIZ:" + yasHesapla2(tarih));
+// //!Funct expression ve arrow func yontemlerinde
+// //! Once fonks tanimlanmalidir sonra cagrilmalidir.
+// //! Aksi takdirde hata alrsiniz.
+
+// //! Func Expression yontemi ile tanimlama
+// const yasHesapla2 = function (tarih) {
+//   const yas = new Date().getFullYear() - tarih;
+//   return yas;
+// };
+// //?-----------------------------------------
+
+// console.log("YASINIZ:" + yasHesapla3(tarih));
+
+// //! Func Declaration yontemi ile tanimlama
+// function yasHesapla3(tarih) {
+//   const yas = new Date().getFullYear() - tarih;
+//   return yas;
+// }
+// //?-----------------------------------------
+
+//* ORNEK: Girilen n. terime kadar Fibonacci sayilarinin toplamini
+//* hesaplayarak yazdiran fonksiyonu dongu ile kodlayiniz.
+//? FIBONACCI terimleri:  1, 1, 2, 3, 5, 8, 13, 21, ...
+
+let stringResult = "1,1";
+// f(n) = f(n-1) + f(n-2)
+const fibo = (n) => {
+  let fib1 = 1;
+  let fib2 = 1;
+  let toplam = 0;
+
+  for (let i = 2; i < n; i++) {
+    toplam = fib1 + fib2;
+    fib1 = fib2;
+    fib2 = toplam;
+    stringResult += "," + fib2;
+  }
+
+  return fib2;
+};
+
+const n = +prompt("n terimini giriniz:");
+if (n <= 0) {
+  console.log("Lutfen 0 dan buyuk bir sayi giriniz.");
+} else {
+  console.log(`FIBO(${n})= ${fibo(n)}`);
+  console.log(stringResult);
+}
+
+// ? =================================================
+// ?           FONKSİYONLAR - RECURSIVE
+// ? ================================================
+
+//* Kendi kendi çağıran fonksiyona recursive fonksiyon denilir.
+//* Özellikle karmasik matematiksel hesaplamalarin kodlamasini
+//* kolaylaştirmak icin kullanilir.
+//* Ancak, dikkatli kullanmak gerekir. Yoksa stack overflow hatasi alınabilir.
+
+//* ORNEK: Girilen n. terime kadar Fibonacci sayılarının toplamini
+//* hesaplayarak yazdiran fonksiyonu recursive olarak kodlayiniz.
+
+console.log("****** 2 - RECURSION ********");
+
+//? FIBONACCI terimleri:  1, 1, 2, 3, 5, 8, 13, 21, ...
+//! f(n) = f(n-1) + f(n-2)
+
+const fibo = (n) => {
+  if (n <= 0) {
+    return 0;
+  } else if (n === 1 || n === 2) {
+    return 1;
+  } else {
+    return fibo(n - 1) + fibo(n - 2);
+  }
+};
+
+const n = +prompt("n terimini giriniz:");
+console.log(`FIBO(${n})= ${fibo(n)}`);
